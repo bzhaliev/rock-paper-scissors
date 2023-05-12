bb0dbdc35913a0058da49e0f25f11853b1f9f027
Bekbolot Zhaliev <bekbolot.zhaliev@gmail.com>
2023-05-11 23:07:17 -0500

    modify index.html modify script.js add style.css

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const computerSelection = getComputerChoice();
+//const computerSelection = getComputerChoice();
 
+const r = document.querySelector('#btn_rock');
+//console.log(r);
+r.addEventListener('click', () => {
+    let playerSelection = 'rock';
+    console.log('Rock selected!');
+    playRound(playerSelection, getComputerChoice());
+    console.log(player);
+    console.log(computer);
+    displayResult();
+});
+
+const p = document.querySelector("#btn_paper");
+//console.log(p);
+p.addEventListener('click', () => {
+    let playerSelection = 'paper';
+    console.log('Paper selected!');
+    playRound(playerSelection, getComputerChoice());
+    console.log(player);
+    console.log(computer);
+    displayResult();
+});
+
+const s = document.querySelector("#btn_scissors");
+//console.log(s);
+s.addEventListener('click', () => {
+    let playerSelection = 'scissors';
+    console.log('Scissors selected!');
+    playRound(playerSelection, getComputerChoice());
+    console.log(player);
+    console.log(computer);
+    displayResult();
+});
 
 function getComputerChoice () {
     const elements = ["rock", "paper", "scissors"];
@@ -8,78 +40,101 @@ function getComputerChoice () {
 }
 
 function playRound(playerSelection, computerSelection) {
+    
     if (playerSelection === "rock" && computerSelection === "paper") {
         console.log("You Lose! Paper beats Rock");
-        return 0;
+        return player += 0, computer += 1;
+        
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("You Win! Rock beats Scissors");
-        return 2;
+        return player += 1, computer += 0;
     }
     else if (playerSelection === "rock" && computerSelection === "rock") {
         console.log("Tie!");
-        return 1;
+        return player += 0, computer += 0;
     }
     else if (playerSelection === "paper" && computerSelection === "paper") {
         console.log("Tie!");
-        return 1;
+        return player += 0, computer += 0;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log("You Lose! Scissors beats Paper");
-        return 0;
+        return player += 0, computer += 1;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("You Win! Paper beats Rock");
-        return 2;
+        return player += 1, computer += 0;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log("You Win! Scissors beats Paper");
-        return 2;
+        return player += 1, computer += 0;
     }
     else if (playerSelection === "scissors" && computerSelection === "scissors") {
         console.log("Tie!");
-        return 1;
+        return player += 0, computer += 0;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log("You Lose! Rock beats Scissors");
-        return 0;
+        return player += 0, computer += 1;
     }
 }
+let player = 0;
+let computer = 0;
 
-function game() {
-    let player = 0;
-    let computer = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Type what you choose:");
-        let result = playRound(playerSelection, computerSelection);
-        if (result === 0) {
-            player = player + 0;
-            computer = computer + 1;
-        }
-        else if (result === 1) {
-            player = player + 1;
-            computer = computer + 1;
-        }
-        else if (result === 2) {
-            player = player + 1;
-            computer = computer + 0;
-        }
-    }
+const content = document.querySelector('#content');
 
-    if (player > computer) {
-        console.log("YOU WON!");
-    }
+const won = document.createElement('div');
+won.textContent = 'YOU WON!';
 
-    else if (player < computer) {
-        console.log("YOU LOST!");
-    }
-    else if (player = computer) {
-        console.log("TIE!")
-    }
+const lost = document.createElement('div');
+lost.textContent = 'YOU LOST!';
 
-    console.log(player);
-    console.log(computer);
+const tie = document.createElement('div');
+tie.textContent = 'TIE!';
+
+function displayResult() {
+    /*if (player == 0 && computer == 0) {
+        content.removeChild(won);
+        content.removeChild(lost);
+        content.removeChild(tie);
+    }*/
+    if (player == 5) {
+        content.appendChild(won);
+        //console.log("YOU WON!");
+        const reset1 = document.createElement('button');
+        reset1.textContent = 'Reset';
+        content.appendChild(reset1);
+        reset1.addEventListener('click', () => {
+            content.removeChild(won);
+            content.removeChild(reset1);
+        });
+        return player = 0, computer = 0;
+        
+    }
+    
+    else if (computer == 5) {
+        content.appendChild(lost);
+        //console.log("YOU LOST!");
+        const reset2 = document.createElement('button');
+        reset2.textContent = 'Reset';
+        content.appendChild(reset2);
+        reset2.addEventListener('click', () => {
+            content.removeChild(lost);
+            content.removeChild(reset2);
+        });
+        return player = 0, computer = 0;
+    }
+    else if (player == 5 && computer == 5) {
+        content.appendChild(tie);
+        //console.log("TIE!");
+        const reset3 = document.createElement('button');
+        reset3.textContent = 'Reset';
+        content.appendChild(reset3);
+        reset3.addEventListener('click', () => {
+            content.removeChild(tie);
+            content.removeChild(reset3);
+        });
+        return player = 0, computer = 0;
+    }
 }
-
-
-console.log(game());
